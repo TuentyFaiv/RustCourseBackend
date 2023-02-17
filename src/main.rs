9b4fe85db6cd8811cda3318b3b64f55d14565a1c
Tonalli López <aguilablanca_7@live.com>
2023-02-17 15:08:52 -0600

    ✨ FEAT: Crud blog

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,171 @@
-#[macro_use]
 use dotenvy::dotenv;
 use std::env;
 
 extern crate diesel;
+extern crate tera;
 
-use diesel::prelude::*;
+use tera::{Tera, Context};
 use diesel::pg::PgConnection;
 use diesel::r2d2::{self, ConnectionManager};
 use diesel::r2d2::Pool;
 
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, put, delete, web, App};
+use actix_web::{HttpResponse, HttpServer, Responder, Result};
 
 pub mod schema;
 pub mod models;
-// pub mod services;
 
-use self::models::Post;
-use self::schema::posts::dsl::*;
-// use self::schema::posts;
-
-// use crate::{services::{post::{create_posts, create_post, update_post, delete_post}}, models::UpdatePost};
+use self::models::{Post, NewPostHandler};
 
 pub type DBPool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
-
 #[get("/")]
-async fn index(pool: web::Data<DBPool>) -> impl Responder {
-
+async fn index(pool: web::Data<DBPool>, html: web::Data<Tera>) -> Result<impl Responder> {
 	let mut conn = pool.get().expect("Error to get database");
-	match web::block(move || {posts.load::<Post>(&mut conn)}).await {
-		Ok(data) => {
 
-			HttpResponse::Ok().body(web::Json(data))
+	let mut ctx = Context::new();
+
+	match web::block(move || {Post::all(&mut conn)}).await {
+		Ok(data) => {
+			let blogs = data.unwrap();
+			ctx.insert("blogs", &blogs);
+
+			let page = html.render("index.html", &mut ctx).unwrap();
+			Ok(HttpResponse::Ok().content_type("text/html").body(page))
 		},
-		Err(data) => HttpResponse::Ok().body("Error to found data"),
+		Err(data) => {
+			let error = format!("Error: {:?}", data);
+			Ok(HttpResponse::Ok().body(error))
+		},
+	}
+}
+
+#[get("/create")]
+async fn create_blog(html: web::Data<Tera>) -> Result<impl Responder> {
+	let mut ctx = Context::new();
+
+	let page = html.render("create.html", &mut ctx).unwrap();
+	Ok(HttpResponse::Ok().content_type("text/html").body(page))
+}
+
+#[get("/update/{slug}")]
+async fn update_blog(
+	pool: web::Data<DBPool>,
+	html: web::Data<Tera>,
+	slug: web::Path<String>
+) -> Result<impl Responder> {
+	let mut conn = pool.get().expect("Error to get database");
+
+	let mut ctx = Context::new();
+
+	match web::block(move || {Post::get(&mut conn, &slug.to_string())}).await {
+		Ok(data) => {
+			let blogs = data.unwrap();
+
+			if blogs.len() == 0 {
+				return Ok(HttpResponse::NotFound().finish());
+			}
+
+			let blog = blogs.get(0).unwrap();
+
+			ctx.insert("blog", blog);
+
+			let page = html.render("update.html", &mut ctx).unwrap();
+			Ok(HttpResponse::Ok().content_type("text/html").body(page))
+		},
+		Err(data) => {
+			let error = format!("Error: {:?}", data);
+			Ok(HttpResponse::Ok().body(error))
+		},
+	}
+}
+
+#[get("/{slug}")]
+async fn show_blog(
+	pool: web::Data<DBPool>,
+	html: web::Data<Tera>,
+	slug_param: web::Path<String>
+) -> impl Responder {
+	let mut conn = pool.get().expect("Error to get database");
+
+	let mut ctx = Context::new();
+
+	match web::block(move || {Post::get(&mut conn, &slug_param.to_string())}).await {
+		Ok(data) => {
+			let blogs = data.unwrap();
+
+			if blogs.len() == 0 {
+				return HttpResponse::NotFound().finish();
+			}
+
+			let blog = blogs.get(0).unwrap();
+
+			ctx.insert("blog", blog);
+
+			let page = html.render("blog.html", &mut ctx).unwrap();
+			HttpResponse::Ok().content_type("text/html").body(page)
+		},
+		Err(data) => {
+			let error = format!("Error: {:?}", data);
+			HttpResponse::Ok().body(error)
+		},
+	}
+}
+
+#[post("/blog/create")]
+async fn new_post(pool: web::Data<DBPool>, item: web::Json<NewPostHandler>) -> impl Responder {
+	let mut conn = pool.get().expect("Error to get database");
+
+	match web::block(move || {Post::create(&mut conn, &item)}).await {
+		Ok(data) => {
+			let data_ok = format!("{:?}", data);
+			HttpResponse::Ok().body(data_ok)
+		},
+		Err(data) => {
+			let error = format!("Error: {:?}", data);
+			HttpResponse::Ok().body(error)
+		},
+	}
+}
+
+#[put("/blog/update/{id}")]
+async fn update_post(
+	pool: web::Data<DBPool>,
+	id: web::Path<String>,
+	item: web::Json<NewPostHandler>
+) -> impl Responder {
+	let mut conn = pool.get().expect("Error to get database");
+
+	match web::block(move || {Post::update(&mut conn, &id.to_string(), &item)}).await {
+		Ok(data) => {
+			let data_ok = format!("{:?}", data);
+			HttpResponse::Ok().body(data_ok)
+		},
+		Err(data) => {
+			let error = format!("Error: {:?}", data);
+			HttpResponse::Ok().body(error)
+		},
+	}
+}
+
+#[delete("/blog/delete/{id}")]
+async fn delete_post(pool: web::Data<DBPool>, id_param: web::Path<String>) -> impl Responder {
+	let mut conn = pool.get().expect("Error to get database");
+
+	match web::block(move || {Post::delete(&mut conn, &id_param.to_string())}).await {
+		Ok(data) => {
+			let data_ok = format!("{}", data.unwrap());
+			HttpResponse::Ok().body(data_ok)
+		},
+		Err(data) => {
+			let error = format!("Error: {:?}", data);
+			HttpResponse::Ok().body(error)
+		},
 	}
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-	// use self::models::{Post, NewPost, PostSimple, UpdatePost};
-	// use self::schema::posts;
-	// use self::schema::posts::dsl::*;
 
 	dotenv().ok();
 	let db_url = env::var("DATABASE_URL").expect("DB url variable not found");
@@ -50,7 +174,18 @@ async fn main() -> std::io::Result<()> {
 	let pool = Pool::builder().build(connection).expect("Can't build Pool");
 
 	HttpServer::new(move || {
-		App::new().service(index).app_data(web::Data::new(pool.clone()))
+		let tera = Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/templates/**/*")).unwrap();
+
+		App::new()
+			.service(index)
+			.service(create_blog)
+			.service(update_blog)
+			.service(show_blog)
+			.service(new_post)
+			.service(update_post)
+			.service(delete_post)
+			.app_data(web::Data::new(pool.clone()))
+			.app_data(web::Data::new(tera))
 	})
 		.bind(("0.0.0.0", 5000))
 		.unwrap()
--- src/models.rs
+++ src/models.rs
@@ -1,19 +1,32 @@
 use diesel::{Queryable, Insertable};
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
+use diesel::result::Error;
+use diesel::r2d2::{ConnectionManager, PooledConnection};
+use serde::{Serialize, Deserialize};
 
 use super::schema::posts;
 
-#[derive(Queryable, Debug)]
+type Conn = PooledConnection<ConnectionManager<PgConnection>>;
+
+#[derive(Queryable, Serialize, Deserialize, Debug)]
 pub struct PostSimple {
   pub title: String,
   pub body: String,
 }
 
-#[derive(Queryable, Debug)]
+#[derive(Queryable, Serialize, Deserialize, Debug)]
 pub struct Post {
   pub id: i32,
   pub title: String,
   pub slug: String,
   pub body: String,
+}
+
+#[derive(Clone, Serialize, Deserialize, Debug)]
+pub struct NewPostHandler {
+  pub title: String,
+  pub body: String
 }
 
 #[derive(Insertable)]
@@ -24,8 +37,70 @@ pub struct NewPost<'a> {
   pub slug: &'a str,
 }
 
-pub struct UpdatePost<'a> {
-  pub title: Option<&'a str>,
-  pub body: Option<&'a str>,
-  pub slug: Option<&'a str>,
+impl Post {
+  pub fn slugify(title: &String) -> String {
+    title
+      .replace(" ", "-")
+      .to_lowercase()
+  }
+
+  pub fn all<'a>(conn: &mut Conn) -> Result<Vec<Post>, Error> {
+    use super::schema::posts::dsl::*;
+
+    posts.load::<Post>(conn)
+  }
+
+  pub fn get<'a>(conn: &mut Conn, slug_post: &String) -> Result<Vec<Post>, Error> {
+    use super::schema::posts::dsl::*;
+
+    posts
+      .filter(slug.eq(slug_post))
+      .load::<Post>(conn)
+  }
+
+  pub fn create<'a>(
+    conn: &mut Conn,
+    post: &NewPostHandler
+  ) -> Result<Post, Error> {
+    let slug = Post::slugify(&post.title.clone());
+
+    let new_post = NewPost {
+      title: &post.title.as_str(),
+      body: &post.body.as_str(),
+      slug: &slug.as_str()
+    };
+
+    diesel::insert_into(posts::table)
+      .values(&new_post)
+      .get_result(conn)
+  }
+
+  pub fn update<'a>(
+    conn: &mut Conn,
+    id_post: &String,
+    post: &NewPostHandler
+  ) -> Result<Post, Error> {
+    use crate::schema::posts::dsl::*;
+
+    let slug_post = Post::slugify(&post.title.clone());
+
+    let id_num = id_post.parse::<i32>().unwrap();
+
+    diesel::update(posts.filter(id.eq(id_num)))
+      .set((
+        title.eq(&post.title.as_str()),
+        body.eq(&post.body.as_str()),
+        slug.eq(&slug_post.as_str()),
+      ))
+      .get_result(conn)
+  }
+
+  pub fn delete<'a>(conn: &mut Conn, id_post: &String) -> Result<usize, Error> {
+    use crate::schema::posts::dsl::*;
+
+    let id_num = id_post.parse::<i32>().unwrap();
+
+    diesel::delete(posts.filter(id.eq(id_num)))
+      .execute(conn)
+  }
 }
